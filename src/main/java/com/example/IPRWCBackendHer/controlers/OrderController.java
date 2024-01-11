4b49f571bbe3083b96a405b85f0d9e1a0b4c8eba
Safin <2003safin@gmail.com>
2024-01-11 22:46:01 +0100

    DELETE: deleted unused classes

--- src/main/java/com/example/IPRWCBackendHer/controlers/OrderController.java
+++ src/main/java/com/example/IPRWCBackendHer/controlers/OrderController.java
@@ -1,35 +1,5 @@
 package com.example.IPRWCBackendHer.controlers;
 
-import com.example.IPRWCBackendHer.DAO.OrderDao;
-import com.example.IPRWCBackendHer.models.ApiResponse;
-import jakarta.persistence.criteria.Order;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.ArrayList;
-
-@Controller
-@RequestMapping(value = "/order")
 public class OrderController {
-    private final OrderDao orderDao;
 
-    public OrderController(OrderDao orderDao) {
-        this.orderDao = orderDao;
-    }
-
-    @RequestMapping(value = "/insert", method = RequestMethod.POST)
-    @ResponseBody
-    public ApiResponse postOrder(@RequestBody ArrayList<Order> orders) {
-        if (orders.size() < 1) {
-            return new ApiResponse(HttpStatus.BAD_REQUEST, "You do not have items in your cart!");
-        }
-        for (Order order : orders) {
-            this.orderDao.saveToDatabase(order);
-        }
-        return new ApiResponse(HttpStatus.ACCEPTED, "You posted some data!");
-    }
 }
--- src/main/java/com/example/IPRWCBackendHer/repository/OrderRepository.java
+++ src/main/java/com/example/IPRWCBackendHer/repository/OrderRepository.java
@@ -1,12 +1,5 @@
 package com.example.IPRWCBackendHer.repository;
 
-import jakarta.persistence.criteria.Order;
-import org.hibernate.validator.constraints.pl.REGON;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, UUID> {
+public interface OrderRepository {
 }
